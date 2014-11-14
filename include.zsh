09bc1d84422bc12cfdd67dce7cea1fa869233c51
Thomas Genin <tgenin@expensify.com>
2014-11-13 22:48:34 -0800

    improve install script

--- include.zsh
+++ include.zsh
@@ -1,3 +1,8 @@
-for filename in expensify/*.zsh; do
+for filename in ~/Dropbox/dotfiles/expensify/*.zsh; do
   source $filename
 done
+
+for filename in ~/Dropbox/dotfiles/git/*.zsh; do
+  source $filename
+done
+
