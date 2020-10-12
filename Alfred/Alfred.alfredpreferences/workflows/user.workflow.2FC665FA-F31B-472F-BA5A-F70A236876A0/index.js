5d3e6aaa6520bf891534125f1f1c46248041be59
Thomas Genin <xt6.thomas.genin@gmail.com>
2020-10-11 18:05:48 -0700

    Add Alfred settings

---
+++ Alfred/Alfred.alfredpreferences/workflows/user.workflow.2FC665FA-F31B-472F-BA5A-F70A236876A0/index.js
@@ -0,0 +1,26 @@
+// const alfy = require('alfy');
+
+// // const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
+
+// // const items = alfy
+// // 	.inputMatches(data, 'title')
+// // 	.map(element => ({
+// // 		title: element.title,
+// // 		subtitle: element.body,
+// // 		arg: element.id
+// // 	}));
+
+// // alfy.output(items.splice(0, 5));
+// var items = [	{
+// 	"title": "fuga nam accusamus voluptas reiciendis itaque",
+// 	"subtitle": "aaa",
+// 	"arg": 36
+// }];
+
+// // alfy.output();
+// console.log(JSON.stringify(items));
+
+const alfy = require('alfy');
+alfy.output([{
+	title: 'Rainbow'
+}]);
